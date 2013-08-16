5694407af2698d23e8d76fcb63bbfc20953a6739
Grant James <gjinfo737@gmail.com>
2013-08-16 14:44:19 -0400

    testing bitmap

--- app/src/com/aj/walls/hot/TheWall.java
+++ app/src/com/aj/walls/hot/TheWall.java
@@ -1,11 +1,14 @@
 package com.aj.walls.hot;
 
+import java.util.Random;
+
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
@@ -34,6 +37,7 @@ public class TheWall extends WallpaperService {
 	}
 
 	public class GalleryEngine extends Engine implements OnSharedPreferenceChangeListener {
+
 		@Override
 		public void onCreate(SurfaceHolder surfaceHolder) {
 			super.onCreate(surfaceHolder);
@@ -53,16 +57,17 @@ public class TheWall extends WallpaperService {
 		}
 
 		private void drawFrame() {
-
 			Rect window = null;
 			Rect surfaceFrame = getSurfaceHolder().getSurfaceFrame();
 			Bitmap bitmapOfFrame = getTestBM();
 			final SurfaceHolder holder = getSurfaceHolder();
+
+			float scale = (float) bitmapOfFrame.getHeight() / (float) surfaceFrame.height();
+
+			float allowedWidth = scale * ((float) bitmapOfFrame.getWidth());
+			int sidePadding = (int) (allowedWidth / 2f);
+
 			window = new Rect(0, 0, bitmapOfFrame.getWidth(), bitmapOfFrame.getHeight());
-
-			// float scale = (float) bitmapOfFrame.getHeight() / (float)
-			// surfaceFrame.height();
-
 			Canvas c = null;
 			try {
 				c = holder.lockCanvas();
@@ -71,16 +76,30 @@ public class TheWall extends WallpaperService {
 					c.drawBitmap(bitmapOfFrame, null, window, null);
 				}
 			} finally {
-				if (c != null)
+				if (c != null) {
 					holder.unlockCanvasAndPost(c);
-			}
 
+				}
+			}
 		}
 
 		private Bitmap getTestBM() {
-			Bitmap createBitmap = Bitmap.createBitmap(30, 30, Config.ARGB_8888);
+			int squareSize = 300;
+			int step = (int) ((float) squareSize * .1f);
+			Bitmap createBitmap = Bitmap.createBitmap(squareSize, squareSize, Config.ARGB_8888);
 			Canvas c = new Canvas(createBitmap);
-			c.drawColor(Color.MAGENTA);
+			c.drawColor(Color.WHITE);
+			Paint paint = new Paint();
+			int[] colors = new int[] { Color.MAGENTA, Color.RED, Color.CYAN, Color.YELLOW, Color.DKGRAY, Color.BLUE, Color.BLACK, Color.WHITE };
+			Random rand = new Random();
+			for (int x = 0; x < createBitmap.getWidth(); x += step) {
+				for (int y = 0; y < createBitmap.getHeight(); y += step) {
+					int color = colors[rand.nextInt(colors.length)];
+					paint.setColor(color);
+					Rect r = new Rect(x, y, x + step, y + step);
+					c.drawRect(r, paint);
+				}
+			}
 			return createBitmap;
 		}
 	}
